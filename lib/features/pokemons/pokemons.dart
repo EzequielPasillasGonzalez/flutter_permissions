561023865004972b57ac3bb1bd73309d91019ea0
Ezequiel Pasillas Gonzalez <ingepasillasg@gmail.com>
2026-03-18 22:50:50 -0600

    fix: importaciones

--- lib/features/pokemons/pokemons.dart
+++ lib/features/pokemons/pokemons.dart
@@ -1,3 +1,4 @@
 export 'domain/datasources/pokemons_datasource.dart';
+export 'domain/';
 export 'presentation/blocs/pokemons_bloc/pokemons_bloc.dart';
 export 'presentation/screens/pokemons_screen.dart';
